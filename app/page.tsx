21785b09bb5121bd471e20d9a00782441ca75f7d
村中信隆 <chanayataka2324@gmail.com>
2024-09-07 18:01:58 +0900

    ログインログアウト機能の実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+"use client";
 import Image from "next/image";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session, status } = useSession();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {status === "authenticated" ? (
+        <div>
+          <p>セッションの期限：{session.expires}</p>
+          <p>ようこそ、{session.user?.name}さん</p>
+          <img
+            src={session.user?.image ?? ``}
+            alt=""
+            style={{ borderRadius: "50px" }}
+          />
+          <div>
+            <Logout />
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
