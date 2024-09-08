0bd0957b7e19c904cebca559f11b5c997366d460
村中信隆 <chanayataka2324@gmail.com>
2024-09-08 20:55:57 +0900

    回答履歴が表示されるようにAPIエンドポイントを設定

--- app/api/answer/[answerId]/route.ts
+++ app/api/answer/[answerId]/route.ts
@@ -1,23 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prismaClient";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { answerId: string } }
-) {
-  const questionId = parseInt(params.answerId, 10);
-
-  if (isNaN(questionId)) {
-    return NextResponse.json({ error: "Invalid question ID" }, { status: 400 });
-  }
-
-  const question = await prisma.question.findUnique({
-    where: { id: questionId },
-  });
-
-  if (!question) {
-    return NextResponse.json({ error: "Question not found" }, { status: 404 });
-  }
-
-  return NextResponse.json(question);
+export async function GET(req: Request) {
+  const allAnswers = await prisma.answer.findMany();
+  return NextResponse.json(allAnswers);
 }
