a04e75052f21cb4eb1356ba36d3b5e1ea39b78bf
村中信隆 <chanayataka2324@gmail.com>
2024-09-08 22:24:31 +0900

    Vercelデプロイ時のエラー解消4

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -5,7 +5,7 @@ const prismaClientSingleton = () => {
 };
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prisma: undefined | PrismaClient;
 }
 
 const prisma = globalThis.prisma ?? prismaClientSingleton();
