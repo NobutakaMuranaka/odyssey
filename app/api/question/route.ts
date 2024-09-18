f081513099d8104f85ea5b59702b019589802838
村中信隆 <chanayataka2324@gmail.com>
2024-09-19 00:25:30 +0900

    質問が id 順で並べ替えられて表示されるように

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -2,6 +2,10 @@ import { NextResponse } from 'next/server';
 import prisma from '../../../lib/prismaClient';
 
 export async function GET() {
-  const allQuestions = await prisma.question.findMany();
+  const allQuestions = await prisma.question.findMany({
+    orderBy: {
+      id: 'asc',
+    },
+  });
   return NextResponse.json(allQuestions);
 }
