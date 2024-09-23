26b42e5e5fa1bed8eacbbba19ab3662b52fae805
村中信隆 <chanayataka2324@gmail.com>
2024-09-23 15:48:00 +0900

    TOPージをLP風に修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import LoginButton from '@/components/LoginButton';
-import Link from 'next/link';
-import { Button } from '../components/ui/button';
+import Image from 'next/image';
 
 export default async function Home() {
   return (
@@ -8,7 +7,7 @@ export default async function Home() {
       <div className="h-full">
         <div className="text-center mt-4 py-28">
           <h1 className="text-5xl">
-            フロントエンド技術の学習サイト
+            React特化の学習サイト
             <br />
             Odyssey
           </h1>
@@ -16,12 +15,46 @@ export default async function Home() {
           <div className="mt-8 mb-4">
             <LoginButton />
           </div>
-          <Link href="/questions">
-            <Button variant="outline">問題一覧</Button>
-          </Link>
-          <p className="text-slate-400 text-xs mt-2">
-            ※ログインしない場合は、回答情報が保存されません。
+        </div>
+        <div className="flex">
+          <div className="m-auto flex gap-5">
+            <Image
+              src="/React_logo.svg"
+              alt="Reactロゴ"
+              width={100}
+              height={40}
+              style={{ width: 'auto', height: 'auto' }}
+            />
+            <Image
+              src="/Nextjs_logo.svg"
+              alt="Nextjsロゴ"
+              width={100}
+              height={40}
+              style={{ width: 'auto', height: 'auto' }}
+            />
+          </div>
+        </div>
+        <div className="text-center p-28 px-96">
+          <h2 className="text-3xl mb-5">Odysseyとは</h2>
+          <h3 className="text-2xl mb-3">特徴1　自己対話型の学習スタイル</h3>
+          <p className="mb-10">
+            Odysseyは、Reactに関する問いを取り上げた哲学ノートです。正解が一つではない問いについて自己対話を通して考え、結論を求めるのではなく、より深い理解を追求します。Odysseyはクイズアプリではなく、単に知識を問うものではありません。Reactのアプリケーション開発における真理を探し、自分自身の思考を整理してノートにまとめることで、深く考える力を養います。
           </p>
+          <h3 className="text-2xl mb-3">特徴2　ノートの記録と成長</h3>
+          <p className="mb-10">
+            Odysseyでは、過去に記録したノートを見返すことで、自己の成長を確認することができます。ノートはあくまで記録として残り、編集はできませんが、「破り捨てる（削除）」ことは可能です。これは、自分の考えやプロセスを大切にし、深く思考することを重視しているためです。
+          </p>
+          <h3 className="text-2xl mb-3">特徴3　シンプルでモダンなデザイン</h3>
+          <p className="mb-10">
+            白と黒を基調にしたモダンなデザインで、広告などを排除し、自己対話に集中できる環境を提供します。
+          </p>
+        </div>
+        <div className="text-center py-28">
+          <h2 className="text-3xl mb-5">答えのない、思考の旅に出よう。</h2>
+          <p>Googleアカウントで簡単ログイン</p>
+          <div className="mt-8 mb-4">
+            <LoginButton />
+          </div>
         </div>
       </div>
     </div>
